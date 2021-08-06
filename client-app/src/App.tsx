e22952a69bf73536ece482828fb91651fbdce358
seanmull <kimlan065@gmail.com>
2021-08-06 16:18:24 -0700

    Add client files

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ducks } from './demo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-          <div key={duck.name}>
-            <span>{duck.name}</span>
-            <button onClick={() => duck.makeSound(duck.name + '  quack')}></button>
-          </div>
-        ))}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
