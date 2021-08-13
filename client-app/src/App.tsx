d5ce3feffb2342ea96a90858abdf7ca5c6e4b948
seanmull <kimlan065@gmail.com>
2021-08-13 15:57:27 -0700

    finished walking skeleton

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -5,26 +5,26 @@ import './App.css';
 //import DuckItem from './DuckItem';
 //TODO install axios in client app folder
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/activities").then(response =>{
+    axios.get("http://localhost:5000/api/activities").then(response =>{
       console.log(response);
       setActivities(response.data);
     })
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          {activities.map((activity:any) => (
-            <li key={activity.id}>
+    <div>
+      <Header as="h2" icon='users' content='Reactivties' />
+        <List>
+        {activities.map((activity:any) => (
+            <List.Item key={activity.id}>
               {activity.title}
-            </li>
+            </List.Item>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }
